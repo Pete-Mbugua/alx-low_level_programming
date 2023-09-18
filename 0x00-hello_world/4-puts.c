c4c3a020c05f7b18fb2facb8e285fb9e7b28582c
Pete-Mbugua <petrembugua@gmail.com>
2023-09-18 14:14:33 +0300

    Script that prints and is followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ * return: 0 Always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
