4be5e1d6547817edc15a76cf4761ce765c40283d
Pete-Mbugua <petrembugua@gmail.com>
2023-09-18 14:19:05 +0300

    Fix checker errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return: 0 Always
+ * return: Always 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
