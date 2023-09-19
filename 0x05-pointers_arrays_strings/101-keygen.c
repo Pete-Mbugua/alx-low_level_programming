978a16e5fa9b8a5de6a678f1fe53d285933693ec
Pete-Mbugua <petrembugua@gmail.com>
2023-09-18 19:03:37 -0700

    Fix checker error

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,43 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define PASSWORD_LENGTH 10
-
 /**
  * main - entry point generates a random password
  *
  * Return: the generated password
  */
-
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-    int i;
+char c;
+int x;
 
-    srand(time(0));
-
-    for (i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        int randomCharType = rand() % 3;
-
-        if (randomCharType == 0)
-        {
-            password[i] = rand() % 10 + '0';
-        }
-        else if (randomCharType == 1)
-        {
-            password[i] = rand() % 26 + 'a';
-        }
-        else
-        {
-            password[i] = rand() % 26 + 'A';
-        }
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("Generated password: %s\n", password);
-
-    return 0;
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
+return (0);
 }
